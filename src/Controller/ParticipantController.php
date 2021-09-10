e1c31016621ece3be56124978830ecb7d648d6cb
Sergey Velmiskin <asdln@ya.ru>
2021-09-10 12:08:42 +0200

    Create participant controller and form type

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ParticipantFormType;
+use App\Repository\StatusRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParticipantController extends AbstractController
+{
+    #[Route('/participant', name: 'participant')]
+    public function index(Request $request, StatusRepository $statusRepository): Response
+    {
+        $form = $this->createForm(ParticipantFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $participant = $form->getData();
+            $status = $statusRepository->findOneBy(['isDefault' => 1]);
+            $participant->setStatus($status);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+        return $this->render('participant/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ParticipantFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use App\Entity\Salutation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('salutation', EntityType::class, [
+                'class' => Salutation::class,
+                'choice_label' => 'title',
+                'attr' => ['class' => 'form-select'],
+            ])
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
