8bb6fe402ed59b2f7e7867355935e279288359da
Sergey Velmiskin <asdln@ya.ru>
2021-09-13 09:26:46 +0200

    Add participant status change test

--- tests/Application/ChangeStatusMessageTest.php
+++ tests/Application/ChangeStatusMessageTest.php
@@ -6,12 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ChangeStatusMessageTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testInitial(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertSelectorTextContains('h1', 'Lottery Application');
+    }
+
+    public function testChangeStatusMessage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', 'admin/edit/0b0a9c3d-d830-4190-b9fd-13af5e0bba33');
+        $form = $crawler->selectButton('admin_participant_form_update')->form();
+        $form['admin_participant_form[status]']->select('176a6c5a-4e12-4c27-97a5-962f8f007e6a');
+        $client->submit($form);
+        $this->assertQueuedEmailCount(1);
     }
 }
