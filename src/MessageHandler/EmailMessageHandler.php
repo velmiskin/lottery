bb60a47db8da53ac64b488facef799e7a658dda5
Sergey Velmiskin <asdln@ya.ru>
2021-09-13 09:25:05 +0200

    Logging email message

--- src/MessageHandler/EmailMessageHandler.php
+++ src/MessageHandler/EmailMessageHandler.php
@@ -3,11 +3,15 @@
 namespace App\MessageHandler;
 
 use App\Message\EmailMessage;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-class EmailMessageHandler implements MessageHandlerInterface
+class EmailMessageHandler implements MessageHandlerInterface, LoggerAwareInterface
 {
+    private LoggerInterface $logger;
+
     public function __construct(private MailerInterface $mailer) {}
 
     /**
@@ -17,5 +21,11 @@ class EmailMessageHandler implements MessageHandlerInterface
     public function __invoke(EmailMessage $email)
     {
         $this->mailer->send($email->getEmail());
+        $this->logger->info('Email send!');
+    }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
     }
 }
